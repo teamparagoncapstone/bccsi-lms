61ebf3f65d2f6646d8ce3bbea75b58e869620b1c
paragoncapstone <paragoncapstone@gmail.com>
2024-11-05 00:39:17 +0800

    aww

--- lib/auditLogger.ts
+++ lib/auditLogger.ts
@@ -1,9 +1,30 @@
 import { prisma } from "@/lib/prisma";
 
 export const logAudit = async (userId: string | null, action: string, entityId: string, details?: string) => {
+    if (!userId) {
+       
+        await prisma.auditLogs.create({
+            data: {
+                userId: null, 
+                userName: "Unknown User", 
+                action,
+                entityId,
+                details,
+            },
+        });
+        return; 
+    }
+
+   
+    const user = await prisma.user.findUnique({
+        where: { id: userId },
+        select: { name: true },
+    });
+
     await prisma.auditLogs.create({
         data: {
-            userId,
+            userId, 
+            userName: user?.name || "Unknown User", 
             action,
             entityId,
             details,
